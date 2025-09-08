91c32192d973a8c770a53c8c6a8ea25ebeaddc32
Vihaan Krishnan <krisvih32@yahoo.com>
2025-09-07 17:53:13 -0700

    gamepad based driving

--- TeamCode/src/main/java/Tutorial.java
+++ TeamCode/src/main/java/Tutorial.java
@@ -1,19 +1,42 @@
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="motor1")
-public class Tutorial extends OpMode {
+@TeleOp(name="forward")
+public class Tutorial extends LinearOpMode {
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
 
-    DcMotor motor1;
     @Override
-    public void init() {
+    public void runOpMode() {
+        // Initialize motors
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
-    }
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motor4 = hardwareMap.get(DcMotor.class, "motor4");
 
-    @Override
-    public void loop() {
-        motor1.setPower(1);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                motor1.setPower(0.25);
+                motor3.setPower(0.25);
+                motor2.setPower(-0.25);
+                motor4.setPower(-0.25);
+            } else if (gamepad1.b) {
+                motor1.setPower(-0.25);
+                motor3.setPower(-0.25);
+                motor2.setPower(0.25);
+                motor4.setPower(0.25);
+            } else {
+                // Stop motors if button not pressed
+                motor1.setPower(0);
+                motor2.setPower(0);
+                motor3.setPower(0);
+                motor4.setPower(0);
+            }
+        }
     }
 }
